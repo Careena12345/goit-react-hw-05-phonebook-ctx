e1e464bf4929934066cade7fb4ef24ec7fe8e1fa
Careena12345 <45653779+Careena12345@users.noreply.github.com>
2020-03-26 21:08:00 +0200

    Updates

---
+++ precache-manifest.51cbb50e4e5419a4c6eb74522da2b914.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed2ec5267094693e1abf3ba31eb71e8",
+    "url": "/goit-react-hw-05-phonebook-ctx/index.html"
+  },
+  {
+    "revision": "a57b33f9f583a93efad4",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/css/main.54f4d59d.chunk.css"
+  },
+  {
+    "revision": "5aa95cc02e60a82b9436",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.03fefe9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.03fefe9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a57b33f9f583a93efad4",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/main.02261496.chunk.js"
+  },
+  {
+    "revision": "489d35a30f18b5dffd57",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/runtime-main.8ba7870d.js"
+  }
+]);
